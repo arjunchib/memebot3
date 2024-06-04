d47fe1f07576240eb35a44e73150d260d0973eb5
Arjun Chib <arjunpatrickchib@gmail.com>
2024-06-04 06:43:26 -0400

    Add save/skip buttons

--- src/controllers/add_controller.ts
+++ src/controllers/add_controller.ts
@@ -1,27 +1,45 @@
-import { ComponentInteraction, button, type $slash } from "peach";
+import {
+  ComponentInteraction,
+  DiscordRestService,
+  button,
+  inject,
+  type $slash,
+} from "peach";
 import type { add } from "../commands";
 import { ffmpeg } from "../ffmpeg";
 import ytdl from "ytdl-core";
 
+let token = "";
+
 export class AddController {
+  private discordRestService = inject(DiscordRestService);
+
   async add(interaction: $slash<typeof add>) {
     const { name, url: youtubeUrl, start, end } = interaction.options;
     // const id = crypto.randomUUID();
     // const audioUrl = await this.getAudioUrl(youtubeUrl);
     // await this.downloadAudio(audioUrl, id, start, end);
-    // await interaction.respondWith(`Previewing *${name}*`);
+    await interaction.respondWith(`Previewing *${name}*`);
     const saveBtn = button("Save").primary().customId("save");
     const skipBtn = button("Skip").secondary().customId("skip");
-    await interaction.respondWith([[saveBtn, skipBtn]]);
+    await interaction.followupWith([[saveBtn, skipBtn]]);
+    token = interaction.raw.token;
   }
 
   async save(interaction: ComponentInteraction) {
     const customId = interaction.raw.data?.custom_id;
     if (customId === "save") {
-      await interaction.respondWith("Saved");
+      await interaction.respondWith({ content: "Saved", components: [] });
     } else {
-      await interaction.respondWith("Skipped");
+      await interaction.respondWith({ content: "Skipped", components: [] });
     }
+    const messageId =
+      interaction.raw.message?.interaction_metadata
+        .original_response_message_id;
+    const channelId = interaction.raw.message?.channel_id;
+    await this.discordRestService.editMessage(channelId!, messageId, {
+      content: "Added",
+    });
   }
 
   private async getAudioUrl(youtubeUrl: string) {
