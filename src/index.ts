586d2f27209b75f9d2fc40f1f734f7dc8549cc34
Arjun Chib <arjunpatrickchib@gmail.com>
2024-08-10 11:19:09 -0400

    Add migration on startup

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,23 @@ import { bootstrap } from "peach";
 import { commands } from "./commands";
 import { routes } from "./routes";
 import { mkdirSync } from "fs";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+import { db } from "./db/database";
 
 try {
   mkdirSync("audio");
 } catch {}
+
+if (process.env.NODE_ENV === "production") {
+  migrate(db, { migrationsFolder: "drizzle" });
+}
 
 await bootstrap({
   applicationId: Bun.env.APPLICATION_ID!,
   token: Bun.env.TOKEN!,
   commands,
   routes,
-  debug: true,
+  debug: process.env.NODE_ENV !== "production",
   syncCommands: {
     guildId: Bun.env.GUILD_ID!,
   },
