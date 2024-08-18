a4cd2c1bfe3e537fb8b7d5b71b9218b0b4521ea6
Arjun Chib <arjunpatrickchib@gmail.com>
2024-08-18 14:56:14 -0400

    Add this year and one off dates

--- src/helpers/parse_date.ts
+++ src/helpers/parse_date.ts
@@ -1,19 +1,75 @@
-import { sub } from "date-fns";
+import {
+  startOfDay,
+  startOfHour,
+  startOfMinute,
+  startOfToday,
+  startOfTomorrow,
+  startOfWeek,
+  startOfYear,
+  startOfYesterday,
+  sub,
+} from "date-fns";
 import wordsToNumbers from "words-to-numbers";
 
 export function parseDate(value: string) {
-  const result =
+  return (
+    parseOneOff(value) ||
+    parseThisX(value) ||
+    parseRelative(value) ||
+    new Date(value)
+  );
+}
+
+function parseOneOff(value: string) {
+  if (value === "today") {
+    return startOfToday();
+  } else if (value === "tomorrow") {
+    return startOfTomorrow();
+  } else if (value === "yesterday") {
+    return startOfYesterday();
+  }
+}
+
+function parseRelative(value: string) {
+  const matches =
     /(?<quantity>.*?)(?<unit> years?| months?| weeks?| days?| hours?| minutes?| seconds?)(?<dir>.*?)/.exec(
       value
     );
-  if (result && result.groups) {
-    const quantity = parseQuantity(result.groups["quantity"].trim());
-    const unit = parseUnit(result.groups["unit"].trim());
-    console.log({ quantity, unit });
+  if (matches && matches.groups) {
+    const quantity = parseQuantity(matches.groups["quantity"].trim());
+    const unit = parseUnit(matches.groups["unit"].trim());
     return sub(new Date(), { [unit]: quantity });
-  } else {
-    return new Date(value);
   }
+}
+
+function parseThisX(value: string) {
+  const matches =
+    /(?<quantity>this|last) (?<unit>year|month|week|day|hour|minute)/.exec(
+      value
+    );
+  if (matches && matches.groups) {
+    const { quantity, unit } = matches.groups;
+    switch (unit) {
+      case "year":
+        return startOfYear(startOf(quantity, unit));
+      case "month":
+        return startOfMinute(startOf(quantity, unit));
+      case "week":
+        return startOfWeek(startOf(quantity, unit));
+      case "day":
+        return startOfDay(startOf(quantity, unit));
+      case "hour":
+        return startOfHour(startOf(quantity, unit));
+      case "minute":
+        return startOfMinute(startOf(quantity, unit));
+    }
+  }
+}
+
+function startOf(quantity: string, unit: string) {
+  return quantity === "last"
+    ? sub(new Date(), { [unit + "s"]: 1 })
+    : new Date();
 }
 
 function parseQuantity(value: string) {
