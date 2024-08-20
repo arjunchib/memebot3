4fdeea5c8f86384b04a920b187e3407995fcf9ae
Arjun Chib <arjunpatrickchib@gmail.com>
2024-08-19 23:25:39 -0400

    Fix playing memes that 404

--- src/controllers/play_controller.ts
+++ src/controllers/play_controller.ts
@@ -16,15 +16,19 @@ export class PlayController {
       return await interaction.respondWith(`404 Meme not found`);
     }
     await interaction.respondWith(`Playing *${meme.name}*`);
-    await this.playAudio(meme.id);
-    await db
-      .update(memes)
-      .set({
-        playCount: meme.playCount + 1,
-        // TODO: remove once this is fixed https://github.com/drizzle-team/drizzle-orm/issues/2388
-        updatedAt: sql`(unixepoch())`,
-      })
-      .where(eq(memes.id, meme.id));
+    try {
+      await this.playAudio(meme.id);
+      await db
+        .update(memes)
+        .set({
+          playCount: meme.playCount + 1,
+          // TODO: remove once this is fixed https://github.com/drizzle-team/drizzle-orm/issues/2388
+          updatedAt: sql`(unixepoch())`,
+        })
+        .where(eq(memes.id, meme.id));
+    } catch (e) {
+      await interaction.editResponse(`Failed to play *${meme.name}*`);
+    }
   }
 
   async random(interaction: $slash<typeof random>) {
@@ -40,28 +44,34 @@ export class PlayController {
         `404 Meme not found (this should not happen)`
       );
     }
-    await interaction.respondWith(`Randomly playing *${meme.name}*`);
-    await this.playAudio(id);
-    await db
-      .update(memes)
-      .set({
-        randomPlayCount: meme.randomPlayCount + 1,
-        // TODO: remove once this is fixed https://github.com/drizzle-team/drizzle-orm/issues/2388
-        updatedAt: sql`(unixepoch())`,
-      })
-      .where(eq(memes.id, id));
+    await interaction.respondWith(`Playing *${meme.name}*`);
+    try {
+      await this.playAudio(id);
+      await db
+        .update(memes)
+        .set({
+          randomPlayCount: meme.randomPlayCount + 1,
+          // TODO: remove once this is fixed https://github.com/drizzle-team/drizzle-orm/issues/2388
+          updatedAt: sql`(unixepoch())`,
+        })
+        .where(eq(memes.id, id));
+    } catch (e) {
+      await interaction.editResponse(`Failed to play *${meme.name}*`);
+    }
   }
 
   private async playAudio(id: string) {
+    const res = await fetch(
+      `${Bun.env.BUCKET_ENDPOINT}/${Bun.env.BUCKET!}/audio/${id}.webm`
+    );
+    if (!res.ok) throw new Error("Cannot find meme");
     const voiceConn = await joinVoice({
       channel_id: Bun.env.CHANNEL_ID!,
       guild_id: Bun.env.GUILD_ID!,
       self_deaf: true,
       self_mute: false,
     });
-    const url = `${Bun.env.BUCKET_ENDPOINT}/${Bun.env
-      .BUCKET!}/audio/${id}.webm`;
-    await voiceConn.playAudio(await fetch(url));
+    await voiceConn.playAudio(res);
     voiceConn.disconnect();
   }
 }
