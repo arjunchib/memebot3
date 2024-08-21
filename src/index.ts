e918b9ab94216185949a856a6b88708fbaa0bf1e
Arjun Chib <arjunpatrickchib@gmail.com>
2024-08-21 00:26:41 -0400

    Remove broken commands

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { bootstrap } from "peach";
 import { commands } from "./commands";
+import { commands as commandsDb, memes } from "./db/schema";
 import { routes } from "./routes";
 import { mkdirSync } from "fs";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { db } from "./db/database";
+import { notInArray } from "drizzle-orm";
 
 try {
   mkdirSync("audio");
@@ -15,6 +17,16 @@ try {
 if (Bun.env.NODE_ENV === "production") {
   migrate(db, { migrationsFolder: "drizzle" });
 }
+
+// remove broken memes
+const memeIds = (await db.query.memes.findMany({ columns: { id: true } })).map(
+  (m) => m.id
+);
+const deleted = await db
+  .delete(commandsDb)
+  .where(notInArray(commandsDb.memeId, memeIds))
+  .returning();
+console.log(`Deleted ${deleted.length} commands`);
 
 await bootstrap({
   applicationId: Bun.env.APPLICATION_ID!,
