d3e15d104f2681bf8af0144f319c335a68b504d5
Arjun Chib <arjunpatrickchib@gmail.com>
2024-08-21 00:14:36 -0400

    Fix missing foreign key pragma

--- src/db/database.ts
+++ src/db/database.ts
@@ -10,6 +10,7 @@ import {
 
 const sqlite = new Database("memebot.sqlite", { create: true });
 sqlite.exec("PRAGMA journal_mode=WAL;");
+sqlite.exec("PRAGMA foreign_keys=ON;");
 export const db = drizzle(sqlite, {
   schema: {
     memes,
